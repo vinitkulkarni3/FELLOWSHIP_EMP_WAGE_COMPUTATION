238728aa0345834141615c1b31cc83ea72a6c7dd
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-15 01:47:29 +0530

    Added: Daily wage computation

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,12 +4,20 @@ public class EmpWageBuilder
 	{
 		System.out.println("WELCOME TO EMP WAGE COMPUTATION PROGRAM");
 
+		//Constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+
+		//variables
+		int empHrs = 0;
+		int empWage = 0 ;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Emp is present");
-		else
-			System.out.println("Emp is absent");
+		{
+			empHrs = 8;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage : "+empWage);
 	}
 }
