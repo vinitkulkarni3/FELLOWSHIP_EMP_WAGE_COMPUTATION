8c2ef6a526fe37be0ffbc963f70dac4cc67cc5ff
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-15 01:59:34 +0530

    Added: Switch case to compute wages

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,30 +1,30 @@
 public class EmpWageBuilder
 {
+	//Constants
+     	public static final int IS_FULL_TIME = 1;
+     	public static final int IS_PART_TIME = 2;
+       	public static final int EMP_RATE_PER_HOUR = 20;
+
 	public static void main(String args[])
 	{
 		System.out.println("WELCOME TO EMP WAGE COMPUTATION PROGRAM");
-
-		//Constants
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 
 		//variables
 		int empHrs = 0;
 		int empWage = 0 ;
 		double empCheck = Math.floor(Math.random() * 10) % 3;
 
-		if (empCheck == IS_FULL_TIME)
+		switch((int)empCheck)
 		{
-			empHrs = 8;
-		}
-		else if(empCheck == IS_PART_TIME)
-		{
-			empHrs = 4;
-		}
-		else
-		{
-			empHrs = 0;
+			case IS_FULL_TIME:
+					empHrs = 8;
+			break;
+			case IS_PART_TIME:
+					empHrs = 4;
+			break;
+			default:
+					empHrs = 0;
+			break;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage : "+empWage);
