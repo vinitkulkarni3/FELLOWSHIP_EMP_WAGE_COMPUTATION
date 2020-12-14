930f53ea1e392fe69511a3a7d5a7ee31f7112436
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-15 01:42:07 +0530

    Added: Emp Present or Absent

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,5 +3,13 @@ public class EmpWageBuilder
 	public static void main(String args[])
 	{
 		System.out.println("WELCOME TO EMP WAGE COMPUTATION PROGRAM");
+
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Emp is present");
+		else
+			System.out.println("Emp is absent");
 	}
 }
